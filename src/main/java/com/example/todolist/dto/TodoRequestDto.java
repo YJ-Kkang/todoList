f3b0d69bfc69b60c263f0ef725b3b19d248a8120
YJ <kyujin995@gmail.com>
2024-12-10 07:47:22 +0900

    refactor: TodoRequestDto 클래스에서 사용하지 않는 부분 제거

--- src/main/java/com/example/todolist/dto/TodoRequestDto.java
+++ src/main/java/com/example/todolist/dto/TodoRequestDto.java
@@ -1,30 +1,12 @@
 package com.example.todolist.dto;
 
-import com.example.todolist.entity.Todo;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 public class TodoRequestDto {
-    private Long id;
     private String task;
     private String name;
     private String password;
-    private LocalDateTime createdOn;
-    private LocalDateTime updatedOn;
 
-    //기본 생성자
-    public TodoRequestDto() {
-        this.createdOn = LocalDateTime.now();
-        this.updatedOn = LocalDateTime.now();
-    }
-
-    //투두 객체가 RequestDto 형태로 바껴서 응답이 되어야 함
-    public TodoRequestDto(Todo todo) {
-        this.id = todo.getId();
-        this.task = todo.getTask();
-        this.name = todo.getName();
-        this.password = todo.getPassword();
-    }
+    public TodoRequestDto() {}
 }
